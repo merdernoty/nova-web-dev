a11f9260379fe00ebf503441c2b27f94d7544354
Roma8642 <141335182+Roma8642@users.noreply.github.com>
2024-01-08 22:47:42 +0200

    fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
-        "montserrat-bold": ["Montserrat-Bold", "sans-serif"],
-        "nunito-sans": ["Nunito Sans", "sans-serif"],
-        "nunito-sans-semibold": ["Nunito Sans SemiBold", "sans-serif"],
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        foreground: "#19191B",
-        fontcolor:"#FFFFFF"
+        'customBlack': '#19191B',
+        'customGrey': '#BABABB',
+        'customPurple': '#262642',
+        'customOrange': '#FBA81C',
+        'customBlue': '#5454D4',
+        'customRed': '#F04037'
       },
+      fontFamily: {
+        'montserrat': ['var(--font-montserrat)', 'Arial', 'sans-serif'],
+        'nunito_sans': ['var(--font-nunito_sans)', 'Arial', 'sans-serif']
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
