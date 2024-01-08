8456a98c49dd9a7128638bfce975b97cd458c132
merdernoty <133769530+merdernoty@users.noreply.github.com>
2024-01-08 18:46:22 +0300

    New Config and background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        "montserrat-bold": ["Montserrat-Bold", "sans-serif"],
+        "nunito-sans": ["Nunito Sans", "sans-serif"],
+        "nunito-sans-semibold": ["Nunito Sans SemiBold", "sans-serif"],
+      },
+      colors: {
+        foreground: "#19191B",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
