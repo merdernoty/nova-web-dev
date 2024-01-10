57c51c00cae027685bdf812d178f2e4bcff36117
Zoom <egor147536987@gmail.com>
2024-01-10 20:28:35 +0300

    commit Fix Footer and Header Finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import Container from './components/Container/Container'
 import Projects from './components/projects/Projects'
 import infoData from './components/data/infoData.json'
 import Info from "./components/Info/Info";
+import Contact from "./components/Contact/Contact";
 export default function Home() {
 	return (
+		<>
 		<Container>
 			<Info mainText={infoData.mainText} subText={infoData.subText} />
 			<Projects projects={projectsData} />
 		</Container>
+		<Contact/>
+		</>
 	)
 }
